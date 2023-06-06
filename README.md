19307b8c7542dd0ab4f4d54be0f65473210f8fd7
Ajay T Shaju <ajaytshaju@gmail.com>
2023-06-06 23:16:40 +0530

    YouTube2MP3

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# YouTube-To-MP3
-A Python program to download audio of a video from YouTube with a GUI. You can put you custom filename and select the folder for the file to be saved 
+<b><h1 align="center"> YouTubeToMP3 </h1></b>
+
+<p align="center">
+<img src="readme\interface.png" title = "Database / SQL" width = "333" height='195'></p>
+
+---
+
+How to make it yours:
+
+* Download the zip of this repo
+* Go to YouTube-To-MP3 > YouTubeToMP3 > dist > YouTubeToMP3 > `YouTubeToMP3.exe`
+
+* Make a shortcut of `YouTubeToMP3.exe` to Desktop (if you need it frequently)
+
+---
+
+How to make it work:
+
+* Paste the link of your YouTube video
+* Give a name
+* Select the output folder
+* Click `Download MP3` 
+
+* Wait for a minute (depends on your video length), the audio file will be downloaded to your selected folder as an MP3 file
---
+++ YouTubeToMP3/YouTubeToMP3.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import time
+import tkinter
+from pytube import YouTube
+import customtkinter as ctk
+from tkinter import filedialog
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+def select_output_folder():
+    global folder_path
+    folder_path = filedialog.askdirectory()  # Open folder selection dialog
+
+def download_mp3():
+    video_url = url.get()
+    filename_text = filename.get()
+
+    try:
+        yt = YouTube(video_url)
+        video = yt.streams.filter(only_audio=True).first()
+        out_file = video.download(output_path=folder_path)
+        new_file = (f"{folder_path}/{filename_text}.mp3")
+        os.rename(out_file, new_file)
+
+        download_info_label.configure(text="MP3 downloaded & saved successfully.")
+        
+        time.sleep(7)
+        root.destroy()
+
+    except:
+        download_info_label.configure(text="Download Error.")
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+root = ctk.CTk()
+root.title("YouTube to MP3")
+root.iconbitmap( resource_path("YM.ico")) # app icon (top left corner)
+root.geometry("500x250")
+
+win_width = root.winfo_screenwidth()
+win_height = root.winfo_screenheight()
+
+frame = ctk.CTkFrame(root, width=win_width/2, height=win_height/2)
+frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+url = ctk.CTkEntry(master=frame, placeholder_text="Enter URL", width=300, fg_color='white', text_color='black')
+url.place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
+
+filename = ctk.CTkEntry(master=frame, placeholder_text="Enter Filename", width=200, fg_color='white', text_color='black')
+filename.place(relx=0.432, rely=0.4, anchor=tkinter.CENTER)
+
+select_output_folder_button = ctk.CTkButton(master=frame, text="Select Output Folder", command=select_output_folder)
+select_output_folder_button.place(relx=0.61, rely=0.4, anchor=tkinter.CENTER)
+
+download_button = ctk.CTkButton(master=frame, text="Download MP3", command=download_mp3)
+download_button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+download_info_label = ctk.CTkLabel(master=frame, text="")
+download_info_label.place(relx=0.5, rely=0.63, anchor=tkinter.CENTER)
+
+root.mainloop()
