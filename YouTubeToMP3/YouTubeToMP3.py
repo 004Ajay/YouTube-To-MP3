a827618b379a78488b75a059d77031ee13af7b93
004Ajay <ajaytshaju@gmail.com>
2026-07-19 15:49:19 +0530

    style: ruff formatting

--- YouTubeToMP3/YouTubeToMP3.py
+++ YouTubeToMP3/YouTubeToMP3.py
@@ -9,6 +9,7 @@ import threading
 
 APP_ICON = Path("img\\icon.ico")  # app icon (top left corner)
 
+
 def resource_path(relative_path):
     try:
         base_path = sys._MEIPASS
@@ -17,6 +18,7 @@ def resource_path(relative_path):
 
     return os.path.join(base_path, relative_path)
 
+
 def select_output_folder():
     global folder_path
     folder_path = filedialog.askdirectory()
@@ -24,6 +26,7 @@ def select_output_folder():
         download_info_label.configure(text=f"Selected folder: {folder_path}")
     else:
         download_info_label.configure(text="No folder selected.")
+
 
 def download_mp3():
     video_url = url.get()
@@ -34,18 +37,19 @@ def download_mp3():
         yt = YouTube(video_url)
         video = yt.streams.filter(only_audio=True).first()
         out_file = video.download(output_path=folder_path)
-        new_file = (f"{folder_path}/{filename_text}.mp3")
+        new_file = f"{folder_path}/{filename_text}.mp3"
         os.rename(out_file, new_file)
 
         download_info_label.configure(text="MP3 downloaded & saved successfully.")
 
-
     except:
         download_info_label.configure(text="Download Error.")
+
 
 def close():
     root.destroy()
     exit(0)
+
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
@@ -58,20 +62,38 @@ root.geometry("500x250")
 win_width = root.winfo_screenwidth()
 win_height = root.winfo_screenheight()
 
-frame = ctk.CTkFrame(root, width=win_width/2, height=win_height/2)
+frame = ctk.CTkFrame(root, width=win_width / 2, height=win_height / 2)
 frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
-url = ctk.CTkEntry(master=frame, placeholder_text="Enter URL", width=300, fg_color='white', text_color='black')
+url = ctk.CTkEntry(
+    master=frame,
+    placeholder_text="Enter URL",
+    width=300,
+    fg_color="white",
+    text_color="black",
+)
 url.place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
 
-filename = ctk.CTkEntry(master=frame, placeholder_text="Enter Filename", width=200, fg_color='white', text_color='black')
+filename = ctk.CTkEntry(
+    master=frame,
+    placeholder_text="Enter Filename",
+    width=200,
+    fg_color="white",
+    text_color="black",
+)
 filename.place(relx=0.432, rely=0.4, anchor=tkinter.CENTER)
 
-select_output_folder_button = ctk.CTkButton(master=frame, text="Select Output Folder", command=select_output_folder)
+select_output_folder_button = ctk.CTkButton(
+    master=frame, text="Select Output Folder", command=select_output_folder
+)
 select_output_folder_button.place(relx=0.61, rely=0.4, anchor=tkinter.CENTER)
 
 # Deamon=True -> Daemon threads automatically terminate when the main thread (Tkinter's main loop) exits.
-download_button = ctk.CTkButton(master=frame, text="Download MP3", command=lambda: threading.Thread(target=download_mp3, daemon=True).start())
+download_button = ctk.CTkButton(
+    master=frame,
+    text="Download MP3",
+    command=lambda: threading.Thread(target=download_mp3, daemon=True).start(),
+)
 download_button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 download_info_label = ctk.CTkLabel(master=frame, text="")
